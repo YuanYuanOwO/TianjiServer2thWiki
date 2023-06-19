8a9fccbc94a2a785fc10e61f553df48645841670
YuanYuanOwO <yuanyuanowo@gmail.com>
2023-06-19 22:10:28 +0800

    Revert "开坑！"
    This reverts commit f30d5b3adc88bc2b9af504ca0d110b0beb3f1b0c.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '天际正版公益服2周目Wiki',
-  tagline: '天际正版公益服2周目Wiki',
+  title: '天际服2周目Wiki',
+  tagline: '天际服2周目Wiki',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
